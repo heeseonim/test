5da3cd293439011daad588744e1620aad35baad6
heeseonim <hisundl@naver.com>
2020-02-06 14:43:18 +0900

    edit user

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -6,10 +6,10 @@ export const login = ({ username, password }) =>
 
 // 회원가입
 export const register = ({ username, password }) =>
-  client.post('/user/signUp', { username, password });
+  client.post('/user/signUp', { uMail:username, uPass:password, uName:null, uParty:null });
 
 // 로그인 상태 확인
-export const check = () => client.get('/api/auth/check');
+export const check = () => client.get('/user/signIn');
 
 // 로그아웃
-export const logout = () => client.post('/api/auth/logout');
+export const logout = () => client.post('/user/signIn');
