adec70f100d0ce1ff97efc4e007d62ff381745ec
heeseonim <hisundl@naver.com>
2020-02-06 15:11:23 +0900

    edit proxy

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const client = axios.create();
 
 // API 주소를 다른 곳으로 사용함
-client.defaults.baseURL = 'http://52.79.219.137:8000/'
+// client.defaults.baseURL = 'http://52.79.219.137:8000/'
 
   // // 헤더 설정
   // client.defaults.headers.common['Authorization'] = 'Bearer a1b2c3d4';
